c9677158ecd18b94a1875dcbab57289e04714f28
Jakob Eigenmann <ga27hib@mytum.de>
2023-08-27 21:23:58 +0200

    Newer links

--- spigot/download-plugins.sh
+++ spigot/download-plugins.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-cd ~/spigot-server/plugins
-
 # ! These links might change in the future
 # WorldGuard
 wget -O worldguard.jar https://dev.bukkit.org/projects/worldguard/files/4675318/download
@@ -11,18 +9,20 @@ wget -O worldguard.jar https://dev.bukkit.org/projects/worldguard/files/4675318/
 wget -O worldedit.jar https://dev.bukkit.org/projects/worldedit/files/4586220/download
 
 # LuckPerms
-wget -O luckperms.jar https://www.spigotmc.org/resources/luckperms.28140/download?version=463843
+wget -O luckperms.jar https://ci.lucko.me/job/LuckPerms/lastStableBuild/artifact/bukkit/loader/build/libs/LuckPerms-Bukkit-5.4.102.jar
 
 # EssentialsX
 wget -O essentialsxcore.jar https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/EssentialsX-2.21.0-dev+13-ee1111f.jar
 wget -O essentialsxchat.jar https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/EssentialsXChat-2.21.0-dev+13-ee1111f.jar
 wget -O essentialsxspawn.jar https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/EssentialsXSpawn-2.21.0-dev+13-ee1111f.jar
+wget -O essentialsxdiscord.jar https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/EssentialsXDiscord-2.21.0-dev+13-ee1111f.jar
+wget -O essentialsxdiscordlink.jar https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/EssentialsXDiscordLink-2.21.0-dev+13-ee1111f.jar
 
 # Vault
-wget -O vault.jar https://www.spigotmc.org/resources/vault.34315/download?version=344916
+wget -O vault.jar https://github.com/MilkBowl/Vault/releases/download/1.7.3/Vault.jar
 
 # MultiverseCore
-wget -O multiversecore.jar https://www.spigotmc.org/resources/multiverse-core.390/download?version=420035
+wget -O multiversecore.jar https://github.com/Multiverse/Multiverse-Core/releases/download/4.3.11/multiverse-core-4.3.11.jar
 
 # Dynmap
 wget -O dynmap.jar https://mediafilez.forgecdn.net/files/4632/182/Dynmap-3.6-spigot.jar
@@ -30,4 +30,4 @@ wget -O dynmap.jar https://mediafilez.forgecdn.net/files/4632/182/Dynmap-3.6-spi
 # Grief Prevention
 wget -O griefprevention.jar https://mediafilez.forgecdn.net/files/4433/61/GriefPrevention.jar
 
-cd ~
+mv ~/*.jar spigot-server/plugins/
